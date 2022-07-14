81c694971c02de1d93dc2ed604a27790c5ff7fe2
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-07-14 09:38:43 -0400

    using authcontext and http request hook to add a new place to db

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,12 +1,19 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth.context';
+import ErrorModal from '../../shared/components/ErrorModal/ErrorModal';
+import LoadingSpinner from '../../shared/components/LoadingSpinner/LoadingSpinner';
 import Input from '../../shared/components/Input/Input';
 import Button from '../../shared/components/Button/Button';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from '../../shared/utils/validators';
-import { useForm } from '../../shared/hooks/form-hook';
 
 const NewPlace = () => {
+  const auth = useContext(AuthContext);
   const [formState, inputHandler] = useForm(
     {
       title: { value: '', isValid: false },
@@ -15,46 +22,64 @@ const NewPlace = () => {
     },
     false
   );
+  const navigate = useNavigate();
+  const { error, isLoading, clearError, sendRequest } = useHttpClient();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
-    // TODO: call to BE
+    try {
+      await sendRequest(
+        'http://localhost:5050/api/places',
+        'POST',
+        { 'Content-Type': 'application/json' },
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          address: formState.inputs.address.value,
+          creator: auth.userId,
+        })
+      );
+      navigate(`/${auth.userId}/places`);
+    } catch (error) {}
   };
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <Input
-        id="title"
-        inputType="input"
-        type="text"
-        label="Title"
-        errorText="Please enter valid title."
-        onInput={inputHandler}
-        validators={[VALIDATOR_REQUIRE()]}
-      />
-      <Input
-        id="description"
-        inputType="textArea"
-        label="Description"
-        errorText="Please enter valid description (at least 5 characters)."
-        onInput={inputHandler}
-        validators={[VALIDATOR_MINLENGTH(5)]}
-      />
-      <Input
-        id="address"
-        inputType="input"
-        label="Address"
-        errorText="Please enter valid address."
-        onInput={inputHandler}
-        validators={[VALIDATOR_REQUIRE()]}
-      />
-      <div className="form-action">
-        <Button type="submit" disabled={!formState.isValid}>
-          ADD PLACE
-        </Button>
-      </div>
-    </form>
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="form" onSubmit={handleSubmit}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          inputType="input"
+          type="text"
+          label="Title"
+          errorText="Please enter valid title."
+          onInput={inputHandler}
+          validators={[VALIDATOR_REQUIRE()]}
+        />
+        <Input
+          id="description"
+          inputType="textArea"
+          label="Description"
+          errorText="Please enter valid description (at least 5 characters)."
+          onInput={inputHandler}
+          validators={[VALIDATOR_MINLENGTH(5)]}
+        />
+        <Input
+          id="address"
+          inputType="input"
+          label="Address"
+          errorText="Please enter valid address."
+          onInput={inputHandler}
+          validators={[VALIDATOR_REQUIRE()]}
+        />
+        <div className="form-action">
+          <Button type="submit" disabled={!formState.isValid}>
+            ADD PLACE
+          </Button>
+        </div>
+      </form>
+    </>
   );
 };
 
