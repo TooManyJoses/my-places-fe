1a81b848942aee24ba32c391bc9e4b6ac823b738
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-07-20 14:38:34 -0400

    authentication with token storage to keep user logged on refresh as long as token is valid

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './shared/components/NavBar';
 import Users from './users/pages/Users';
@@ -8,18 +8,59 @@ import NewPlace from './places/pages/NewPlace';
 import Auth from './users/pages/Auth';
 import { AuthContext } from './shared/context/auth.context';
 
+let logoutTimer;
+
 function App() {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-  const login = useCallback((userId, token) => {
+  const login = useCallback((userId, token, expirationDate) => {
     setToken(token);
     setUserId(userId);
+    const tokenExpiration =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 120);
+    setTokenExpirationDate(tokenExpiration);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId,
+        token,
+        tokenExpiration: tokenExpiration.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem('userData');
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTokenTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTokenTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout]);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    if (
+      userData &&
+      userData.token &&
+      new Date(userData.tokenExpiration) > new Date()
+    ) {
+      login(
+        userData.userId,
+        userData.token,
+        new Date(userData.tokenExpiration)
+      );
+    }
   }, []);
 
   let routes;
